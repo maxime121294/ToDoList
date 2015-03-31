e070d7c9e3924461db65e5cf72ab34325ded3604
Maxime REBIBO <maximerebibo@gmail.com>
2015-03-31 19:52:12 +0200

    Création de la page d'accueil (index)

--- src/ToDoList/ListBundle/Controller/DefaultController.php
+++ src/ToDoList/ListBundle/Controller/DefaultController.php
@@ -3,11 +3,22 @@
 namespace ToDoList\ListBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+	/**
+     * Affiche la page d'accueil contenant les listes de l'utilisateur connecté
+     *
+     * @Route("/", name="listes")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
     {
-        return $this->render('ToDoListListBundle:Default:index.html.twig', array('name' => $name));
+    	$user = $this->container->get('security.context')->getToken()->getUser();
+        return $this->render('ToDoListListBundle:Default:index.html.twig', array('name' => $user->getUsername()));
     }
 }
