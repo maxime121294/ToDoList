09fc297840d21acd7bc2f75a45c7a286ed40006e
Maxime REBIBO <maximerebibo@gmail.com>
2015-04-07 00:58:13 +0200

    Envoi des données du formulaire de nouvelle tache vers la base de données

--- src/ToDoList/ListBundle/Controller/DefaultController.php
+++ src/ToDoList/ListBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use ToDoList\ListBundle\Form\TaskType;
+use ToDoList\ListBundle\Entity\Task;
 
 class DefaultController extends Controller
 {
@@ -33,14 +34,29 @@ class DefaultController extends Controller
      */
     public function addAction(Request $request)
 	{
+		$user = $this->container->get('security.context')->getToken()->getUser();
+		$form = $this->createForm(new TaskType());
 		// Si la requête est en POST, c'est que le visiteur a soumis le formulaire
-		if ($request->isMethod('POST')) {
-			// Ici, on s'occupera de la création et de la gestion du formulaire
+		if ($request->isMethod('POST'))
+		{
+
+			$form->handleRequest($request);
+			$data = $form->getData();
+
+			if ($form->isValid())
+			{
+				$data->setAuthor($user);
+				// On l'enregistre notre objet $advert dans la base de données, par exemple
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($data);
+				$em->flush();
+
+				return $this->redirect($this->generateUrl('listes'));
+			}
 			// Puis on redirige vers l'index
-			return $this->redirect($this->generateUrl('listes'));
+			return $this->redirect($this->generateUrl('ajouter_tache'));
 		}
 
-		$form = $this->createForm(new TaskType());
 		// Si on n'est pas en POST, alors on affiche le formulaire
 		return $this->render('ToDoListListBundle:List:add.html.twig', array(
 			'form' => $form->createView(),
