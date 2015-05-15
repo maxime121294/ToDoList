b59d8d7f929d88ddd73be500d4835cc14e3b4639
Maxime REBIBO <maximerebibo@gmail.com>
2015-05-16 01:35:07 +0200

    Suppression de toutes les démos fournies avec Symfony2

--- src/ToDoList/UserBundle/Controller/DefaultController.php
+++ src/ToDoList/UserBundle/Controller/DefaultController.php
@@ -8,12 +8,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
-    {
-        return array('name' => $name);
-    }
+	/**
+	 * @Route("/", name="homepage")
+	 */
+	public function indexAction()
+	{
+		$user = $this->container->get('security.context')->getToken()->getUser();
+
+		if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_ANONYMOUSLY'))
+		{
+			return $this->redirect($this->generateUrl('fos_user_security_login'));
+		}
+		else
+		{
+			return $this->redirect($this->generateUrl('listes'));
+		}
+	}
 }
